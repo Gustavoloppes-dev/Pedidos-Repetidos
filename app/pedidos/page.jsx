eef5c67101908e1eb221060fd5f7faf0bb22cab9
Gustavo Lopes <gustavoloppes.dev@gmail.com>
2025-08-04 22:37:17 -0300

    feat: enviando id na prop deletar

--- app/pedidos/page.jsx
+++ app/pedidos/page.jsx
@@ -1,32 +1,8 @@
-"use client"
-
-import { useEffect, useState } from "react";
-import { getBooks } from "../components/boo";
 
 
 export default function RelatorioPage() {
-  const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getBooks();
-        setBooks(data);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-    fetchData();
-  }, []);
-
   return (
-    <div>
-      <h1>Lista de Livros</h1>
-      <ul>
-        {books.map((book, index) => (
-          <li key={index}>{book[0]}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    <h1>RELATORIO TESTE!</h1>
+
+  )
 }
